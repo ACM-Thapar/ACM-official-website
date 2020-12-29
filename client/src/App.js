d79ffa13e1d72dea66223fed8e0f621c168a8d21
Suprit Behera <supritb18@tutanota.com>
2020-12-29 15:59:27 +0530

    Add Team Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Team from './components/team/Team';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <section>
-          <Switch></Switch>
+          <Switch>
+            <Route exact path="/team" component={Team} />
+          </Switch>
         </section>
       </div>
     </Router>
