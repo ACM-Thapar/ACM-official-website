0068d07eae08e71198f0fd79e2ed3868cdf2c2ea
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-17 19:24:30 +0530

    made changes to team-page

--- theme/js/carousel.js
+++ theme/js/carousel.js
@@ -1,11 +1,31 @@
-var indexToGet = $('.slider .slick-slide').index($('#center_on_me'));
-
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  infinite: true,
-  centerMode: true,
-  slidesToScroll: 1,
-  initialSlide: indexToGet,
-  dots: true,
-  focusOnSelect: true,
+var viewport_width = window.innerWidth;
+$(window).resize(function () {
+  viewport_width = window.innerWidth;
+  window.location.reload();
 });
+if (viewport_width > 500) {
+  var indexToGet = $('.slider .slick-slide').index($('#center_on_me'));
+
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    infinite: true,
+    centerMode: true,
+    slidesToScroll: 1,
+    initialSlide: indexToGet,
+    dots: true,
+    focusOnSelect: true,
+  });
+} else {
+  var indexToGet = $('.slider .slick-slide').index($('#center_on_me'));
+
+  $('.slider-nav').slick({
+    slidesToShow: 1.65,
+    infinite: true,
+    centerMode: true,
+    slidesToScroll: 1,
+    initialSlide: indexToGet,
+    dots: true,
+    focusOnSelect: true,
+  });
+  $('.slick-slide').style.width = 300;
+}
