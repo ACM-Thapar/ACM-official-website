ac1d30beb0e7ae08c825004e6000a9cf3a98298d
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-05-12 10:12:26 +0530

    removed comments

--- server/controllers/auth_post.js
+++ server/controllers/auth_post.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-//const config = require('config')
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
--- server/controllers/user_post.js
+++ server/controllers/user_post.js
@@ -4,7 +4,6 @@ const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-//const config = require('config')
 require('dotenv').config();
 
 //@route    POST api/users
