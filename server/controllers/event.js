9c1a7c191b87f789c93ec855268b1234d871d9b5
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-12 13:13:30 +0530

    Added route to add event

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Blog = require('../models/Blog');
+const Event = require('../models/Event');
 
 //@route    GET /event
 //@desc     Get events
@@ -12,5 +12,27 @@ exports.getEvent = async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
+  }
+};
+
+//@route    POST /event/add
+//@desc     Add an event
+//@access   Private
+
+exports.addEvent = async (req, res) => {
+  try {
+    const newEvent = new Event({
+      Name: req.body.Name,
+      Description: req.body.Description,
+      Time: req.body.Time,
+      StartDate: req.body.StartDate,
+      EndDate: req.body.EndDate,
+    });
+
+    const event = await newEvent.save();
+    res.json(event);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
   }
 };
