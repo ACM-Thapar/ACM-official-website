4ade83a458d47594401c1dd49f30cf6d5a39740d
Suprit Behera <supritb12@gmail.com>
2021-07-13 15:35:04 +0530

    Update Footer component

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -8,27 +8,51 @@ const Footer = () => {
           <div className="row">
             <div className="col-md-8">
               <div className="social-icons">
-                <a className="social-icon social-icon--github">
+                <a
+                  className="social-icon social-icon--github"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-github" />
                   <div className="tooltip">Github</div>
                 </a>
-                <a className="social-icon social-icon--twitter">
+                <a
+                  className="social-icon social-icon--twitter"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-twitter" />
                   <div className="tooltip">Twitter</div>
                 </a>
-                <a className="social-icon social-icon--dribbble">
+                <a
+                  className="social-icon social-icon--dribbble"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-dribbble" />
                   <div className="tooltip">Dribbble</div>
                 </a>
-                <a className="social-icon social-icon--instagram">
+                <a
+                  className="social-icon social-icon--instagram"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-instagram" />
                   <div className="tooltip">Instagram</div>
                 </a>
-                <a className="social-icon social-icon--linkedin">
+                <a
+                  className="social-icon social-icon--linkedin"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-linkedin" />
                   <div className="tooltip">LinkedIn</div>
                 </a>
-                <a className="social-icon social-icon--facebook">
+                <a
+                  className="social-icon social-icon--facebook"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer">
                   <i className="fa fa-facebook" />
                   <div className="tooltip">Facebook</div>
                 </a>
