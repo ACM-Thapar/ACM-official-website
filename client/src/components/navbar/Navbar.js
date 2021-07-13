5a10ed30899e242579e36d653160004d7cad8c3a
Suprit Behera <supritb12@gmail.com>
2021-07-13 15:51:32 +0530

    Update NavBar component

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -3,55 +3,52 @@ import logo from '../../images/logo.svg';
 const Navbar = () => {
   return (
     <div>
-      <header>
-        {/* //Navbar */}
-        <nav className="navbar navbar-expand-lg shift fixed-top">
-          <a className="navbar-brand" href="#">
-            <img src={logo} alt />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" style={{ color: 'black' }} />
-          </button>
-          <div className="col-md-4" />
-          <div className="col-md-3" />
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item main">
-                <a className="nav-link" href="#">
-                  Events
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Team
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Announcements
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+      <nav className="navbar navbar-expand-lg shift fixed-top navbar-light bg-light">
+        <a className="navbar-brand acmlogonav" href="./index.html">
+          <img src={logo} alt="ACM Logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="col-md-4" />
+        <div className="col-md-3" />
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="./team.html">
+                Team
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./project.html">
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./event.html">
+                Events
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="./blogs.html">
+                Blogs
+              </a>
+            </li>
+            <li className="nav-item main">
+              <a className="nav-link" href="#">
+                Achievements
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
