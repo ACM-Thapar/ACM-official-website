99c269668307ebc329140b24cc3f24cd703365d4
Suprit Behera <supritb12@gmail.com>
2021-07-17 14:20:30 +0530

    Comment out cruip external CSS stylesheet CDN

--- client/src/components/external-css/CruipThemeWrapper.js
+++ client/src/components/external-css/CruipThemeWrapper.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
 export const CruipThemeWrapper = styled.div`
-  @import url(https://preview.cruip.com/solid/dist/css/style.css);
+  ${'' /* Uncomment to use cruip stylesheet as a styled-component */}
+  ${'' /* @import url(https://preview.cruip.com/solid/dist/css/style.css); */}
 `;
