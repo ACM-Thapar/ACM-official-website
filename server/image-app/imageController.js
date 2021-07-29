c4d2fbd706d4fb94420924ee3764ff6501e68968
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-29 19:44:28 +0530

    upolad images for events

--- server/image-app/imageController.js
+++ server/image-app/imageController.js
@@ -1,6 +1,7 @@
 var imageModel = require('./models');
 var cloud = require('./cloudinaryConfig');
 const Member = require('../models/Member');
+const Event = require('../models/Event');
 
 exports.createApp = (req, res) => {
   try {
@@ -38,6 +39,49 @@ exports.createApp = (req, res) => {
             { $set: { ImgURL: imageDetails.cloudImage } },
           );
           res.json(imageDetails.cloudImage);
+        });
+      }
+    });
+  } catch (execptions) {
+    console.log(execptions);
+  }
+};
+
+exports.createEventApp = (req, res) => {
+  try {
+    var imageDetails = {
+      imageName: req.body.imageName,
+    };
+
+    imageModel.find({ imageName: imageDetails.imageName }, (err, callback) => {
+      if (err) {
+        console.log(err);
+        res.json({
+          err: err,
+          message: 'there was a problem uploading image',
+        });
+      } else {
+        var imageDetails = {
+          imageName: req.files[0].originalname,
+          cloudImage: req.files[0].path,
+          imageId: '',
+        };
+        cloud.uploads(imageDetails.cloudImage).then(async (result) => {
+          let event = await Event.findOne({ _id: req.params._id });
+
+          if (!event) return res.status(400).json({ msg: 'Profile Not found' });
+
+          var imageDetails = {
+            imageName: req.body.imageName,
+            cloudImage: result.url,
+            imageId: result.id,
+          };
+
+          event = await Event.findOneAndUpdate(
+            { _id: req.params._id },
+            { $push: { ImgURL: imageDetails.cloudImage } },
+          );
+          res.json(event.ImgURL);
         });
       }
     });
