a8464b1a377a95c66c078fcd09cda6552b853d4e
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-29 20:34:29 +0530

    upolad images for blog

--- server/image-app/imageController.js
+++ server/image-app/imageController.js
@@ -2,6 +2,7 @@ var imageModel = require('./models');
 var cloud = require('./cloudinaryConfig');
 const Member = require('../models/Member');
 const Event = require('../models/Event');
+const Blog = require('../models/Blog');
 
 exports.createApp = (req, res) => {
   try {
@@ -69,7 +70,7 @@ exports.createEventApp = (req, res) => {
         cloud.uploads(imageDetails.cloudImage).then(async (result) => {
           let event = await Event.findOne({ _id: req.params._id });
 
-          if (!event) return res.status(400).json({ msg: 'Profile Not found' });
+          if (!event) return res.status(400).json({ msg: 'Event Not found' });
 
           var imageDetails = {
             imageName: req.body.imageName,
@@ -82,6 +83,49 @@ exports.createEventApp = (req, res) => {
             { $push: { ImgURL: imageDetails.cloudImage } },
           );
           res.json(event.ImgURL);
+        });
+      }
+    });
+  } catch (execptions) {
+    console.log(execptions);
+  }
+};
+
+exports.createBlogApp = (req, res) => {
+  try {
+    var imageDetails = {
+      imageName: req.body.imageName,
+    };
+
+    imageModel.find({ imageName: imageDetails.imageName }, (err, callback) => {
+      if (err) {
+        console.log(err);
+        res.json({
+          err: err,
+          message: 'there was a problem uploading image',
+        });
+      } else {
+        var imageDetails = {
+          imageName: req.files[0].originalname,
+          cloudImage: req.files[0].path,
+          imageId: '',
+        };
+        cloud.uploads(imageDetails.cloudImage).then(async (result) => {
+          let blog = await Blog.findOne({ _id: req.params._id });
+
+          if (!blog) return res.status(400).json({ msg: 'Blog Not found' });
+
+          var imageDetails = {
+            imageName: req.body.imageName,
+            cloudImage: result.url,
+            imageId: result.id,
+          };
+
+          blog = await Blog.findOneAndUpdate(
+            { _id: req.params._id },
+            { $push: { ImgURL: imageDetails.cloudImage } },
+          );
+          res.json(imageDetails.cloudImage);
         });
       }
     });
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const imageController = require('../image-app/imageController');
+const upload = require('../image-app/cloudinaryUploads/multer');
 
 const { getBlog, postBlog } = require('../controllers/blog');
 
 router.get('/', getBlog);
 
 router.post('/add', [auth], postBlog);
+
+router.post(
+  '/addImage/:_id',
+  [auth],
+  upload.any(),
+  imageController.createBlogApp,
+);
 
 module.exports = router;
