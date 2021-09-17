91e8b8ff000027a886760e117cf137664f8c4157
gunjeevsingh <gunjeev.arneja@gmail.com>
2021-09-17 17:01:23 +0530

    Projects page basic structure

---
+++ client/src/components/projects/ProjectCard.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useState } from 'react';
+import { Card, Button, Modal } from 'react-bootstrap';
+import DummyProjectImage from '../../images/project.png';
+import './projects.css';
+
+export default function ProjectCard() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <div>
+      <Card style={{ width: '18rem' }} className="card procard">
+        <img
+          src={DummyProjectImage}
+          className="card-img-top pcimg "
+        />
+        <div className="card-body">
+          <p className="card-title promonth">DEC</p>
+          <h4 className="card-title prodate">25</h4>
+          <h4 className="card-title protitle">Website Design Project</h4>
+          <a
+            href="#exampleModal"
+            data-toggle="modal"
+            className="stretched-link cardmodallinkevent"
+            onClick={handleShow}
+          />
+          <p className="card-text prodescription small">
+            Time - 8:00 AM - 10:00 PM <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ...
+          </p>
+        </div>
+      </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <div className="modal-content mcpro">
+          <img src={DummyProjectImage} classNamw="modal-img mipro" alt="" />
+          <div className="main-content">
+            <h3 className="modal-heading mhpro">Web Design Project</h3>
+            <div className="datepro">
+              <p className="monthpro">DEC</p>
+              <p className="dtpro">25</p>
+            </div>
+            <div className="paraspro">
+              <p>
+                Folly words widow one downs few age every seven. If miss part by
+                fact he park just shew.
+                <br />
+                <br />
+                Discovered had get considered projection who favourable.
+                Necessary up knowledge it tolerably. Unwilling departure
+                education is be dashwoods or an. Use off agreeable law unwilling
+                sir deficient curiosity instantly. Easy mind life fact with see
+                has bore ten.
+                <br />
+                <br />
+                Parish any chatty can elinor direct for former. Up as meant
+                widow equal an share least.
+              </p>
+            </div>
+            <div className="teampro">
+              <h2>TEAM</h2>
+              <div className="rowmodal">
+                <div className="col-2 pc2pro">
+                  <img src="./images/face1.png" alt />
+                </div>
+                <div className="pc2textpro">
+                  <p className="randompro">Random Name</p>
+                  <p className="positionpro">POSITION</p>
+                </div>
+              </div>
+              <div className="rowmodal">
+                <div className="col-2 pc2pro">
+                  <img src="./images/face2.png" alt />
+                </div>
+                <div className="pc2textpro">
+                  <p className="randompro">Random Name</p>
+                  <p className="positionpro">POSITION</p>
+                </div>
+              </div>
+              <div className="rowmodal">
+                <div className="col-2 pc2pro">
+                  <img src="./images/face3.png" alt />
+                </div>
+                <div className="pc2textpro">
+                  <p className="randompro">Random Name</p>
+                  <p className="positionpro">POSITION</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
--- client/src/components/projects/Projects.js
+++ client/src/components/projects/Projects.js
@@ -1,289 +1,65 @@
 import React from 'react';
+import { useState } from 'react';
 import ProjectDummyImage from '../../images/project.png';
-import { CruipThemeWrapper } from '../external-css/CruipThemeWrapper';
+import ProjectGif from '../../images/web_developing.gif'
+import { Card } from 'react-bootstrap'; 
+import ProjectCard from './ProjectCard';
+import './projects.css';
 
 const Projects = () => {
+
+
   return (
     <>
-      <div>
-        {/* Solid Animation */}
-        <section className="projectspagetop">
-          <div className="container">
-            <div className="row justify-content-end">
-              <div className="col-4">
-                <h1 className="pmainsectiontitle">Projects</h1>
-                <p className="mainsectiontextpro">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit ...
-                  tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit ... tempor
-                  incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor
-                  sit amet, consectetur adipiscing elit ... tempor incididunt ut
-                  labore et dolore magna aliqua.
-                </p>
-              </div>
-              <div className="projectspagegif col-6">
-                <img src="./images/web_developing.gif" alt />
-              </div>
-            </div>
-          </div>
-        </section>
-        <CruipThemeWrapper>
-          <section className="propagecards">
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-4">
-                  <div
-                    className="card procard lpcard lpcard1"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src={ProjectDummyImage}
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title prodate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div
-                    className="card procard cpcard cpcard1"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src="./images/project.png"
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title prodate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div
-                    className="card procard rpcard rpcard1"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src="./images/project.png"
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title prodate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row justify-content-center">
-                <div className="col-4">
-                  <div
-                    className="card procard lpcard lpcard2"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src="./images/project.png"
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title prodate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div
-                    className="card procard cpcard cpcard2"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src="./images/project.png"
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title prodate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <div
-                    className="card procard rpcard rpcard2"
-                    style={{ width: '18rem' }}>
-                    <img
-                      src="./images/project.png"
-                      className="card-img-top pcimg"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <p className="card-title promonth">DEC</p>
-                      <h4 className="card-title eventdate">25</h4>
-                      <h4 className="card-title protitle">
-                        Website Design Project
-                      </h4>
-                      <a
-                        href="#exampleModal"
-                        data-toggle="modal"
-                        className="stretched-link cardmodallinkevent"
-                      />
-                      <p className="card-text prodescription small">
-                        Time - 8:00 AM - 10:00 PM <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-          {/* Modal */}
-          <div
-            className="modal fade pmodal"
-            id="exampleModal"
-            tabIndex={-1}
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered justify-content-center">
-              <div className="modal-content mcpro">
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close">
-                  <span aria-hidden="true">×</span>
-                </button>
-                <img
-                  src="./images/image 4.png"
-                  className="modal-img mipro"
-                  alt
-                />
-                <div className="main-content">
-                  <h3 className="modal-heading mhpro">Web Design Project</h3>
-                  <div className="datepro">
-                    <p className="monthpro">DEC</p>
-                    <p className="dtpro">25</p>
-                  </div>
-                  <div className="paraspro">
-                    <p>
-                      Folly words widow one downs few age every seven. If miss
-                      part by fact he park just shew.
-                      <br />
-                      <br />
-                      Discovered had get considered projection who favourable.
-                      Necessary up knowledge it tolerably. Unwilling departure
-                      education is be dashwoods or an. Use off agreeable law
-                      unwilling sir deficient curiosity instantly. Easy mind
-                      life fact with see has bore ten.
-                      <br />
-                      <br />
-                      Parish any chatty can elinor direct for former. Up as
-                      meant widow equal an share least.
-                    </p>
-                  </div>
-                  <div className="teampro">
-                    <h2>TEAM</h2>
-                    <div className="rowmodal">
-                      <div className="col-2 pc2pro">
-                        <img src="./images/face1.png" alt />
-                      </div>
-                      <div className="pc2textpro">
-                        <p className="randompro">Random Name</p>
-                        <p className="positionpro">POSITION</p>
-                      </div>
-                    </div>
-                    <div className="rowmodal">
-                      <div className="col-2 pc2pro">
-                        <img src="./images/face2.png" alt />
-                      </div>
-                      <div className="pc2textpro">
-                        <p className="randompro">Random Name</p>
-                        <p className="positionpro">POSITION</p>
-                      </div>
-                    </div>
-                    <div className="rowmodal">
-                      <div className="col-2 pc2pro">
-                        <img src="./images/face3.png" alt />
-                      </div>
-                      <div className="pc2textpro">
-                        <p className="randompro">Random Name</p>
-                        <p className="positionpro">POSITION</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </CruipThemeWrapper>
+ <section className="projectspagetop">
+  <div className="container">
+    <div className="row justify-content-end">
+      <div className="col-4">
+        <h1 className="pmainsectiontitle">Projects</h1>
+        <p className="mainsectiontextpro">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ... tempor
+          incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit ... tempor incididunt ut labore
+          et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit ... tempor incididunt ut labore et dolore magna
+          aliqua.
+        </p>
       </div>
+      <div className="projectspagegif col-6">
+        <img src={ProjectGif} style={{width:'100%'}}/>
+      </div>
+    </div>
+  </div>
+</section>
+
+      <div className="container card-cont">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+            <ProjectCard />
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+          <ProjectCard />
+
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+          <ProjectCard />
+
+          </div>
+
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+            <ProjectCard />
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+          <ProjectCard />
+
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12 col-12 my-5 disp-flex">
+          <ProjectCard />
+
+          </div>
+        </div>
+      </div>
+      
     </>
   );
 };
