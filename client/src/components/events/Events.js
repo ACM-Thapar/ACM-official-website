c7d92ba7c8e1f8c393e361b008e8f9b0fe135ae7
Suprit Behera <supritb12@gmail.com>
2021-09-19 21:22:15 +0530

    Add event card component

--- client/src/components/events/Events.js
+++ client/src/components/events/Events.js
@@ -5,20 +5,18 @@ import ProjectGif from '../../images/web_developing.gif';
 import { Card } from 'react-bootstrap';
 import EventCard from './EventCard';
 const Events = () => {
-  const dummyData = [
-    {
-      month: 'DEC',
-      date: '25',
-      title: 'Website Design Project',
-      cardBriefDescription:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      cardImageSrc:
-        'https://res.cloudinary.com/kriti711/image/upload/v1626162626/ACM%20Website/image_2_vbzq6k.svg',
+  const dummyData = {
+    month: 'DEC',
+    date: '25',
+    cardTitle: 'Website Design Project',
+    cardBriefDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    cardImageSrc:
+      'https://res.cloudinary.com/kriti711/image/upload/v1626162626/ACM%20Website/image_2_vbzq6k.svg',
 
-      mainDescription:
-        'Folly words widow one downs few age every seven. If miss part by fact he park just shew.\n\n Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten.\n\n Parish any chatty can elinor direct for former. Up as meant widow equal an share least. \n\n Folly words widow one downs few age every seven. If miss part by fact he park just shew.',
-    },
-  ];
+    mainDescription:
+      'Folly words widow one downs few age every seven. If miss part by fact he park just shew.\n\n Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten.\n\n Parish any chatty can elinor direct for former. Up as meant widow equal an share least. \n\n Folly words widow one downs few age every seven. If miss part by fact he park just shew.',
+  };
   return (
     <>
       <section className="projectspagetop">
