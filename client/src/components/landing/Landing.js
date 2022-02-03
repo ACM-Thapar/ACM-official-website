a15b9dd4f61ed98bd2b3ea9bea54f90faf07659f
Samik Malhotra <samikmalhotra66@gmail.com>
2022-02-03 18:32:12 +0530

    updated landing and navbar

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -18,11 +18,8 @@ const Landing = () => {
               />
               <h1 className="ACMT">ACM THAPAR</h1>
               <p className="maintext">
-                ACM offers the resources, access and tools to invent the future.
-                No one has a larger global network of professional peers. No one
-                has more exclusive content. No one presents more forward-looking
-                events. Or confers more prestigious awards. Or provides a more
-                comprehensive learning center.
+              ACM is the world’s largest computing society with the motive of “Advancing Computing as a science and profession”
+              At Thapar ACM Student Chapter, we aim towards the holistic development of our members by collaborating and engaging in a plethora of technical activities and projects where brainstorming and strategic planning is the primary key to excel.
               </p>
               <button className="learn-more">
                 <span className="circle" aria-hidden="true">
@@ -101,7 +98,7 @@ const Landing = () => {
                     d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
                   />
                 </svg>
-                <a href="#" className="learnmorenew">
+                <a href="https://www.acm.org/membership" target='_blank' className="learnmorenew">
                   {' Learn More'}
                 </a>
               </button>
